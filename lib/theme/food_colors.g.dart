c5899e0407a3170d17f7ad883b300c64380bb5fe
Parabeac-Nest <admin@parabeac.com>
2022-11-16 18:57:06 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/food_colors.g.dart
@@ -0,0 +1,14 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class FoodColors {
+
+static const Color lineIcon = Color(0xff33363f);
+
+static const Color gray1 = Color(0xff333333);
+}
